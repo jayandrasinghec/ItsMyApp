24a707e53bb84d304ed27e8581894a65d216579d
jayandrasinghec <jayandrasinghec@gmail.com>
2016-05-06 14:48:49 +0530

    Routing for signin and signup.

---
+++ Web/javascripts/app.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+var itsMyApp = angular.module('ItsMyApp', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/signin', {
+                templateUrl: 'views/signin.html',
+                controller: signInController
+            })
+            .when('/signup', {
+                templateUrl: 'views/signup.html',
+                controller: signInController
+            })
+            .otherwise({ redirectTo: '/signin' });
+
+    }
+]);
+
+var signInController = function ($scope, $rootScope, $location) {
+    $scope.btnSignUpClick = function() {
+        $location.path("/signup");
+        $location.replace();
+    }
+};
+
+var signUpController = function($scope, $rootScope) {
+    $scope.hello = 'Hello';
+};
